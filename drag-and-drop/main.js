054039063611c33f75af4bb5319db0690a61afab
Robert Leedy <leedyrj@gmail.com>
2018-06-15 14:38:43 -0500

    can only add one card. working on dragging back

--- drag-and-drop/main.js
+++ drag-and-drop/main.js
@@ -1,3 +1,9 @@
+// The user should be able to drag one of the middle cards into either the top box, or the bottom box. However, there's a problem with the way the code currently works. There's also a couple changes you need to make.
+
+// If you drag one of the cards into the top/bottom box, and then drag another card into the first one, you get a nested card. You need to prevent this from happening.
+// The user should only be able to drag one card into either box. Use the childNodes property to ensure that, if a card is already in the box, another can't be added.
+// The user should be able to move a card from the top/bottom box back to the middle.
+
 const DragDropManager = Object.create(null, {
     init: {
         value: () => {
@@ -27,7 +33,9 @@ const DragDropManager = Object.create(null, {
                     // Append card to target component as child
                     // TODO: This should only happen if the target has no children nodes
                     // TODO: This should not happen if the target is another stage card
+                    if (e.target.hasChildNodes() === false) {
                     e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+                    }
                 }
             })
         }
